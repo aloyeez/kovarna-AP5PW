338147ff5991e0005d73cd591d6b3ef8ff50e72d
aloyeez <prokipchukkateryna@gmail.com>
2025-09-21 00:04:36 +0200

    modify SecurityConfig and enable testing

--- backend_kovarna/src/main/java/com/example/backend_kovarna/infrastructure/config/SecurityConfig.java
+++ backend_kovarna/src/main/java/com/example/backend_kovarna/infrastructure/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.example.backend_kovarna.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // for simple testing
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login.disable()); // for now without login form
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
