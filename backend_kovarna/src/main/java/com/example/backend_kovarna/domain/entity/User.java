4e7ca621669b01427a9ea7101650f3bde8b26e6f
aloyeez <prokipchukkateryna@gmail.com>
2025-09-20 16:45:10 +0200

    add User entity and add relationship mapping between User and Role entities

--- backend_kovarna/src/main/java/com/example/backend_kovarna/domain/entity/User.java
+++ backend_kovarna/src/main/java/com/example/backend_kovarna/domain/entity/User.java
@@ -1,5 +1,38 @@
 package com.example.backend_kovarna.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
