a32c596ba55e7567ac14fe884e1444d7488f9b03
aloyeez <prokipchukkateryna@gmail.com>
2025-09-25 13:25:31 +0200

    add method for finding reservation slot by time

--- backend_kovarna/src/main/java/com/example/backend_kovarna/infrastructure/repository/ReservationSlotRepository.java
+++ backend_kovarna/src/main/java/com/example/backend_kovarna/infrastructure/repository/ReservationSlotRepository.java
@@ -3,5 +3,8 @@ package com.example.backend_kovarna.infrastructure.repository;
 import com.example.backend_kovarna.domain.entity.ReservationSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalTime;
+
 public interface ReservationSlotRepository extends JpaRepository<ReservationSlot, Long> {
+    boolean existsBySlotFromAndSlotTo(LocalTime slotFrom, LocalTime slotTo);
 }
