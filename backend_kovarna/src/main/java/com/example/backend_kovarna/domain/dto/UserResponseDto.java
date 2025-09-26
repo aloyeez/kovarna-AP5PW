b2744bdd42e0fb5ee79ac881fd37bc9409be21a5
aloyeez <prokipchukkateryna@gmail.com>
2025-09-26 11:16:19 +0200

    modify user response structure

---
+++ backend_kovarna/src/main/java/com/example/backend_kovarna/domain/dto/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.backend_kovarna.domain.dto;
+
+import com.example.backend_kovarna.domain.entity.User;
+import lombok.Getter;
+
+@Getter
+public class UserResponseDto {
+    private final Long id;
+    private final String username;
+    private final String email;
+
+    public UserResponseDto(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
+}
