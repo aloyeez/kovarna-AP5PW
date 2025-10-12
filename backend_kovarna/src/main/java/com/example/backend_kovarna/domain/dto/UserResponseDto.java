5fbdefd98849a27cb8898c449d2503fbe58c9316
aloyeez <prokipchukkateryna@gmail.com>
2025-10-12 14:55:49 +0200

    Include user roles in UserResponseDto

--- backend_kovarna/src/main/java/com/example/backend_kovarna/domain/dto/UserResponseDto.java
+++ backend_kovarna/src/main/java/com/example/backend_kovarna/domain/dto/UserResponseDto.java
@@ -1,17 +1,23 @@
 package com.example.backend_kovarna.domain.dto;
 
+import com.example.backend_kovarna.domain.entity.Role;
 import com.example.backend_kovarna.domain.entity.User;
 import lombok.Getter;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class UserResponseDto {
     private final Long id;
     private final String username;
     private final String email;
+    private final Set<String> roles;
 
     public UserResponseDto(User user) {
         this.id = user.getId();
         this.username = user.getUsername();
         this.email = user.getEmail();
+        this.roles = user.getRoles().stream().map(Role::getName).collect(Collectors.toSet());
     }
 }
